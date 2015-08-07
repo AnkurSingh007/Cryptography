81d8038d7a8a99f374a31ea84a45dd321f465da9
AnkurSingh007 <AnkurSingh007@users.noreply.github.com>
2015-08-08 01:26:45 +0530

    Update DES_encryption.c

--- DES_encryption.c
+++ DES_encryption.c
@@ -37,75 +37,75 @@ static const int FINAL_PERMUTATION[64] = {
     33, 1, 41, 9, 49, 17, 57, 25
 };
 
-static const int EXPENSION_PERMUTATION[8][6] = {
-    { 32, 1, 2, 3, 4, 5 },
-    { 4, 5, 6, 7, 8, 9 },
-    { 8, 9, 10, 11, 12, 13 },
-    { 12, 13, 14, 15, 16, 17 },
-    { 16, 17, 18, 19, 20, 21 },
-    { 20, 21, 22, 23, 24, 25 },
-    { 24, 25, 26, 27, 28, 29 },
-    { 28, 29, 30, 31, 32, 1 }
+static const int EXPENSION_PERMUTATION[48] = {
+     32, 1, 2, 3, 4, 5 ,
+     4, 5, 6, 7, 8, 9 ,
+     8, 9, 10, 11, 12, 13 ,
+     12, 13, 14, 15, 16, 17 ,
+     16, 17, 18, 19, 20, 21 ,
+     20, 21, 22, 23, 24, 25 ,
+     24, 25, 26, 27, 28, 29 ,
+     28, 29, 30, 31, 32, 1
 };
 
-static const int PERMUTATION[8][4] = {
-    { 16, 7, 20, 21 },
-    { 29, 12, 28, 17 },
-    { 1, 15, 23, 26 },
-    { 5, 18, 31, 10 },
-    { 2, 8, 24, 14 },
-    { 32, 27, 3, 9 },
-    { 19, 13, 30, 6 },
-    { 22, 11, 4, 25 }
+static const int PERMUTATION[32] = {
+     16, 7, 20, 21 ,
+     29, 12, 28, 17,
+    1, 15, 23, 26 ,
+     5, 18, 31, 10 ,
+     2, 8, 24, 14 ,
+     32, 27, 3, 9 ,
+     19, 13, 30, 6 ,
+     22, 11, 4, 25
 };
 
-static const int SBOX1[4][16] = {
+static  int SBOX1[4][16] = {
     { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
     { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
     { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
     { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 }
 };
-static const int SBOX2[4][16] = {
+static  int SBOX2[4][16] = {
     { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
     { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
     { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
     { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 }
 };
-static const int SBOX3[4][16] = {
+static  int SBOX3[4][16] = {
     { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
     { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
     { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
     { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 }
 };
-static const int SBOX4[4][16] = {
+static  int SBOX4[4][16] = {
     { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
     { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
     { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
     { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 }
 };
 
-static const int SBOX5[4][16] = {
+static  int SBOX5[4][16] = {
     { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
     { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
     { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
     { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 }
 };
 
-static const int SBOX6[4][16] = {
+static  int SBOX6[4][16] = {
     { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
     { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
     { 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
     { 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 }
 };
 
-static const int SBOX7[4][16] = {
+static int SBOX7[4][16] = {
     { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
     { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
     { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
     { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 }
 };
 
-static const int SBOX8[4][16] = {
+static  int SBOX8[4][16] = {
     { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
     { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
     { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
@@ -152,21 +152,43 @@ void get_message(unsigned char* message);
 unit generate_key();
 token rotate_key_left(token t, int shift);
 
+token fiestal(token message,  unit key);
+uint64_t sbox(uint64_t input, int  box[][16]);
 
 int main()
 {
     unit* unit_arr;
     unit key;
     int size = 0;
-	unit * round_keys;
+    int i = 0;
+    int j = 0;
+	   unit * round_keys;
     tokenize_message(&unit_arr, &size);
     key = generate_key();
 	  round_keys = generate_roundkeys(key);
-
     initial_permutation(unit_arr, size);
     print_message(unit_arr, size);
-  //  key = permutation_c1(key);
+    for(i = 0; i < size; i++){
+        uint32_t * leftptr = (uint32_t *)&(unit_arr[i].a);
+        uint32_t * rightptr = (uint32_t *)&(unit_arr[i].b);
+        uint32_t * tempptr;
+        uint32_t * holder;
+        token temp;
+
+        for(j == 0; j < 16; j++){
+              temp = fiestal(*(token *)rightptr, round_keys[j]);
+              holder = (uint32_t * )&temp;
+              *leftptr ^= *holder;
+              //exchanging pointer
+              tempptr = leftptr;
+              leftptr = rightptr;
+              rightptr= tempptr;
+        }
+        unit_arr[i].a = *(token *)leftptr;
+        unit_arr[i].b = *(token *)rightptr;
+    }
     final_permutation(unit_arr, size);
+
     print_message(unit_arr, size);
 
     return 0;
@@ -378,16 +400,6 @@ unit*  generate_roundkeys(unit key)
 	unit * key_arr = (unit *)malloc(sizeof(unit) * 16);
 	unit k = key;
 	 uint64_t * tracekey = ( uint64_t *)&k;
-	printf("printing key\n");
-	for(i = 0; i < 64; i++){
-		if(*tracekey & (1L << (63 - i)))
-				printf("1");
-		else
-				printf("0");
-	}
-	printf("\n");
-
-	printf("working on roundkey function\n");
 	token t1, t2;
 	uint32_t * trace = (uint32_t *)&t1;
 	//generating first token C0
@@ -402,13 +414,7 @@ unit*  generate_roundkeys(unit key)
 			*trace &= ~(1 << (31 - i));
 		}
 	}
-  printf("printing  C0\n");
   trace = (uint32_t *)&t1;
-  for(i = 0; i < 32; i++){
-    if(*trace & (1<<(31 - i)))printf("1");
-    else printf("0");
-  }
-  printf("\n");
 	//generating second token D0
   trace = (uint32_t *)&t2;
 	for(i = 32; i < 64; i++){
@@ -421,83 +427,57 @@ unit*  generate_roundkeys(unit key)
 		}
 	}
   trace = (uint32_t *)&t2;
-  printf("printing for D0\n");
-  for(i = 0; i < 32; i++){
-    if(*trace & (1<<(31 - i)))printf("1");
-    else printf("0");
-  }
-  printf("\n");
 	//tested till here successfully
 	//working on producing round keys
 	for(i = 0; i <= 16 ; i++){
-      printf("printing for i = %d\n",i);
 			if(i == 0){
 				t1 = rotate_key_left(t1, LEFT_SHIFTS[0]);
-        printf("printing for t1 after shift\n");
         trace = (uint32_t *)&t1;
-        for(j = 0; j < 32; j++){
-          if(*trace & (1<<(31 - j)))printf("1");
-          else printf("0");
-        }
-        printf("\n");
 				t2 = rotate_key_left(t2, LEFT_SHIFTS[0]);
-        printf("printing for t2 after shift\n");
         trace = (uint32_t *)&t2;
-        for(j = 0; j < 32; j++){
-          if(*trace & (1<<(31 - j)))printf("1");
-          else printf("0");
-        }
-        printf("\n");
 			}
 			else{
-          printf("entered into else\n");
 					t1 = rotate_key_left(t1, LEFT_SHIFTS[i - 1]);
 					trace = (uint32_t *)&t1;
-          printf("printing for t1 after shift\n");
           trace = (uint32_t *)&t1;
-          for(j = 0; j < 32; j++){
-            if(*trace & (1<<(31 - j)))printf("1");
-            else printf("0");
-          }
-          printf("\n");
 					t2 = rotate_key_left(t2, LEFT_SHIFTS[i - 1]);
 					uint64_t * pointer;
-					unit temp;
-					temp.a= t1;temp.b = t2;
-					pointer = (uint64_t *)&temp;
-					uint32_t * tokenpointer = (int32_t *)&t1;
-					for(j = 0; j < 28; j++){
-						if(*tokenpointer & (1 <<(31 - j)))*pointer |= (1L << (63 - j));
-						else *pointer &= ~(1 << (63 - j));
-					}
-					tokenpointer = (uint32_t *)&t2;
-					for(j = 28; j < 56; j++){
-						if(*tokenpointer & (1<<(59 - j)))*pointer |= (1L << (63 - j));
-						else *pointer &= ~(1 << (63 - j));
-					}
+          unit result = {0};
+          pointer = (uint64_t *)&result;
+          char arr[56];
+          uint32_t * tokenpointer = (uint32_t *)&t1;
+          for(j = 0; j < 28; j++){
+            if(*tokenpointer & (1<<(31 - j)))arr[j] = 1;
+            else arr[j]= 0;
+          }
+          tokenpointer = (uint32_t *)&t2;
+          for(j = 28; j < 56; j++){
+            if(*tokenpointer & (1<<(59 - j)))arr[j] = 1;
+            else arr[j]= 0;
+          }
+
 					//temp now contains 56 bits
 					//permutating
-					unit result;
-					uint64_t * resultpointer = (uint64_t * )&result;
-					for(j = 0; j < 56; j++){
-						if(j < 48){
-							if(*tokenpointer & (1<<(56 - PERMUTATION_CHOICE2[j])))
-								*resultpointer |= (1L << (63 - j));
+          char arr2[j];
+					for(j = 0; j < 48; j++){
+							if(arr[PERMUTATION_CHOICE2[j] -  1])
+								arr2[j] = 1;
 							else
-								*resultpointer &= ~(1 << (63 - j));
+								arr2[j] = 0;;
 						}
-						else{
-							*resultpointer &= ~(1 << (63 - j));
-						}
-					 }
-           printf("printing for resultpointer after permutation\n");
-           resultpointer = (uint32_t *)&t2;
-           for(j = 0; j < 32; j++){
-             if(*trace & (1<<(31 - j)))printf("1");
-             else printf("0");
-           }
-           printf("\n");
-				   key_arr[i - 1] = result;
+            unsigned long long * ptr;
+           ptr = (unsigned long long *)&result;
+           for(j = 0; j < 48; j++){
+               if(arr2[j]){
+                 *ptr |= (1ull <<(63 - j));
+              }
+               else
+                 *ptr &= ~(1ull <<(63 - j));
+            }
+            for(j = 48; j < 64; j++){
+              *ptr &= ~(1ull <<(63 - j));
+            }
+  				   key_arr[i - 1] = result;
 			}
 	}
 	return key_arr;
@@ -522,4 +502,101 @@ token rotate_key_left(token t, int shift){
 	}
 	free(shifts);
 	return here;
+}
+
+token fiestal(token message,  unit key){
+      //key_arr unit has 48 significant bits to use
+      //expanding token
+      int i = 0;
+      unit temp ;
+      token expended;
+      expended = message;
+      uint32_t * pointer1 = (uint32_t *)&expended;
+      uint64_t * pointer2 = (uint64_t *)&key;
+      uint64_t tempint;
+      char arr[48];
+      uint32_t uiholder = 1;
+      uint64_t ullholder = 1;
+      for(i = 0; i < 48; i++){
+          if(*pointer1 & (uiholder << (32 - EXPENSION_PERMUTATION[i])))
+            tempint |= (ullholder<<(63 - i));
+          else
+             tempint &= ~(ullholder<<(63 - i));
+      }
+      tempint ^= *pointer2;
+      uint64_t final = 0;
+      uint64_t holder;
+      holder = sbox(tempint, SBOX1);
+      holder >>=60;
+      final |= holder;
+      final <<= 4;
+      holder = sbox(tempint, SBOX2);
+      holder >>= 60;
+      final |= holder;
+      final |= holder;
+      final<<=4;
+      holder = sbox(tempint, SBOX3);
+      holder >>= 60;
+      final |= holder;
+      final<<=4;
+      holder = sbox(tempint, SBOX4);
+      holder >>= 60;
+      final |= holder;
+      final<<=4;
+      holder = sbox(tempint, SBOX5);
+      holder >>= 60;
+      final |= holder;
+      final<<=4;
+      holder = sbox(tempint, SBOX6);
+      holder >>= 60;
+      final |= holder;
+      final<<=4;
+      holder = sbox(tempint, SBOX7);
+      holder >>= 60;
+      final |= holder;
+      final<<=4;
+      holder = sbox(tempint, SBOX8);
+      holder >>= 60;
+      final |= holder;
+
+      token returntoken;
+      uint32_t * returnptr = (uint32_t *)&returntoken;
+      //permutating final
+      for(i = 0; i < 32; i++){
+          if(holder & (uiholder << (31 - i)))*returnptr |= (uiholder << (31 - i));
+          else *returnptr &= ~(uiholder << (31 - i));
+      }
+      return returntoken;
+}
+
+
+uint64_t sbox(uint64_t input, int  box[][16]){
+    //input has 6 relevent bits
+    int i = 0;
+    uint64_t inputholder = input;
+    uint32_t row = 0;
+    uint32_t col = 0;
+    uint32_t returnval = 0;
+    uint32_t uiholder = 1;
+    uint64_t ullhodler = 1;
+    //getting row
+    if(inputholder & (ullhodler << 63))row |= (uiholder<<1);
+    if(inputholder & (ullhodler << 58))row |= (uiholder);
+    //getting column
+    if(inputholder & (ullhodler << 62))col |= (uiholder<<3);
+    if(inputholder & (ullhodler << 61))col |= (uiholder<<2);
+    if(inputholder & (ullhodler << 60))col |= (uiholder<<1);
+    if(inputholder & (ullhodler << 59))col |= (uiholder);
+    //putting 4 bits in return unit
+    returnval = *(uint32_t *)&box[row][col];
+    for(i = 0; i < 63; i++){
+      if(i < 4){
+        if(returnval & (uiholder << (3 - i)))inputholder |= (ullhodler << (63 - i));
+        else inputholder &= ~(ullhodler << (63 - i));
+      }
+      else{
+        inputholder &= ~(ullhodler << (63 - i));
+      }
+    }
+    return inputholder;
 }
